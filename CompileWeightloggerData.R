eb47ebc7e992e58fb7938fc1f142f190340c9455
hentatisundberg <jonas.sundberg@slu.se>
2022-06-21 20:06:22 +0200

    Removed plots and cleaned up code

--- CompileWeightloggerData.R
+++ CompileWeightloggerData.R
@@ -5,8 +5,9 @@
 # ******************************************************* #
 
 # ******************************************************* #
-# Working directories
+# Local working directory
 macdir <- "~/Library/Mobile Documents/com~apple~CloudDocs/Documents/data/work - synced/"
+
 
 # ******************************************************* #
 # Install packages
@@ -26,7 +27,6 @@ allfiles = list.files(path)
 df = data.frame()
 for (i in allfiles) {
   p2 = paste(path, (i), sep = "")
-  #print(p2)
   p3 = read.csv(p2, sep = ";")
   df = rbind.fill(df, p3)
 }
@@ -51,6 +51,33 @@ for (i in 1:end) {
 rollmin_spangaps = approx(rollmin, xout = 1:length(rollmin), method = "constant")$y
 df$rollmin = rollmin_spangaps
 
-#p = ggplot(data = df, aes(x = DateTime)) + geom_line(aes(y = rollmin)) + geom_line(aes(y = Far3), col = "red", size = .2) + theme_classic(); p
-#pp = ggplotly(p)
-#pp
+
+
+# Weight events
+datx = df[!is.na(df$Far3c),]
+datx = df[df$Far3c > -.1 & df$Far3c < 2,]
+
+birdon = ifelse(datx$Far3c > .7, 1, 0)
+event = diff(birdon); event[event == -1] <- 0; event[is.na(event)] <- 0
+eventnum = cumsum(event)
+eventnum[birdon == 0] <- NA
+datx$event = eventnum
+
+# Stats for each each weight event 
+s1 = aggregate(cbind(DateTime, Far3c) ~ event, data = datx, FUN = max); s1$wmax = s1$Far3c
+s2 = aggregate(cbind(DateTime, Far3c) ~ event, data = datx, FUN = min); s2$wmin = s2$Far3c
+s3 = aggregate(Far3c ~ event, data = datx, FUN = median); s3$wmedian = s3$Far3c
+s4 = aggregate(Far3c ~ event, data = datx, FUN = function(x) quantile(x, probs = .8)); s4$w08 = s4$Far3c
+s1$End = as.POSIXct(s1$DateTime, origin = "1970-01-01")
+s2$Start = as.POSIXct(s2$DateTime, origin = "1970-01-01")
+evdat = data.frame(s1$event, s2$Start, s1$End, s2$wmin, s1$wmax, s3$wmedian, s4$w08)
+colnames(evdat) = c("event", "Start", "End", "wmin", "wmax", "wmedian", "w08")
+evdat$Dur = as.numeric(difftime(evdat$End, evdat$Start))
+evdat$Day = format(evdat$Start, "%m-%d")
+
+
+# Weight as a function of time of the day etc. 
+evdat$H = as.numeric(format(evdat$Start, "%H"))
+evdat$daynum = as.numeric(format(evdat$Start, "%j"))
+evdat$day = as.Date(evdat$Start)
+
