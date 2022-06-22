6441bcdca326930e4ddb51e58ef8acc9a06acf73
hentatisundberg <jonas.sundberg@slu.se>
2022-06-22 12:31:13 +0200

    Generalized code for handling several scales

--- CompileWeightloggerData.R
+++ CompileWeightloggerData.R
@@ -28,61 +28,79 @@ df = data.frame()
 for (i in allfiles) {
   p2 = paste(path, (i), sep = "")
   p3 = read.csv(p2, sep = ";")
+  p3$DateTime = as.POSIXct(paste(p3$Date, p3$Time), format = "%d/%m/%Y %H:%M:%S")
+  p3 = p3[!duplicated(p3$DateTime),]
   df = rbind.fill(df, p3)
 }
 
-df = df[,c(1, 2, 4:6)]
-df$DateTime = as.POSIXct(paste(df$Date, df$Time), format = "%d/%m/%Y %H:%M:%S")
+df = df[,c(15, 4:6, 17)]
+df = df[!is.na(df[,"Far3"]),]
+
+# Change data format
 df$Far3 = as.numeric(df$FARALLON3)
 df$Bon1 = as.numeric(df$BONDEN1)
 df$Toms = as.numeric(df$TOMS)
+df$Far8D = as.numeric(df$FAR8D)
 
-# New calibration method - forward rolling min value
-inputdata = df$Far3 
 
-windowsize = 1500
-rollmin = vector(length = length(inputdata))
-end = length(inputdata - windowsize)
-for (i in 1:end) {
-  rollmin[i] = min(inputdata[i:(i+windowsize)])
+# Post-calibration function for scales that are being re-calibrated because of bird colissions or other reasons
+calib_scale = function(inputdata, windowsize) {
+  rollmin = vector(length = length(inputdata))
+  end = length(inputdata - windowsize)
+  for (i in 1:end) { rollmin[i] = min(inputdata[i:(i+windowsize)]) }
+  
+  # Span gaps in rollmin
+  rollmin_spangaps = approx(rollmin, xout = 1:length(rollmin), method = "constant")$y
+  
+  # Calibrated data 
+  output = inputdata - rollmin_spangaps
+  
+  # Return
+  return(output)
+    
 }
 
-# Span gaps in rollmin
-rollmin_spangaps = approx(rollmin, xout = 1:length(rollmin), method = "constant")$y
-df$rollmin = rollmin_spangaps
+# Calibrate Far3 scale
+df$Far3c = calib_scale(df$Far3, 1500)
 
 
-# Calibrated data 
-df$Far3c = df$Far3 - df$rollmin
+inputdata = df$Toms 
 
 
+# Function for defining weight events 
+eventnum = function(inputdata, threshold) {
+  birdon = ifelse(inputdata > threshold, 1, 0)
+  event = diff(birdon); event[event == -1] <- 0; event[is.na(event)] <- 0
+  eventnum = cumsum(event)
+  eventnum[birdon == 0] <- NA
+  
+  return(eventnum)
+}
 
-# Weight events
-datx = df[!is.na(df$Far3c),]
-datx = df[df$Far3c > -.1 & df$Far3c < 2,]
 
-birdon = ifelse(datx$Far3c > .7, 1, 0)
-event = diff(birdon); event[event == -1] <- 0; event[is.na(event)] <- 0
-eventnum = cumsum(event)
-eventnum[birdon == 0] <- NA
-datx$event = eventnum
+# Define events 
+df$F3event = eventnum(df$Far3c, .7) 
+df$TLevent = eventnum(df$Toms, .7)
+df$F8event = eventnum(df$Far8D, .7)
+df$B1event = eventnum(df$Bon1, .7)
 
 
+# Long format better for aggregating data for separate ledges 
+dflong = data.frame(DateTime = rep(df$DateTime, 4), Ledge = rep(c("Far3", "Bon1", "Toms", "Far8"), each = nrow(df)), rbind(data.frame(w = df$Far3, e = df$F3event), data.frame(w = df$Bon1, e = df$B1event), data.frame(w = df$Toms, e = df$TLevent), data.frame(w = df$Far8, e = df$F8event)))
+
+# Remove NA
+dflong = dflong[!is.na(dflong$DateTime) & dflong$e > 0,]
 
 # Stats for each each weight event 
-s1 = aggregate(cbind(DateTime, Far3c) ~ event, data = datx, FUN = max); s1$wmax = s1$Far3c
-s2 = aggregate(cbind(DateTime, Far3c) ~ event, data = datx, FUN = min); s2$wmin = s2$Far3c
-s3 = aggregate(Far3c ~ event, data = datx, FUN = median); s3$wmedian = s3$Far3c
-s4 = aggregate(Far3c ~ event, data = datx, FUN = function(x) quantile(x, probs = .8)); s4$w08 = s4$Far3c
-s1$End = as.POSIXct(s1$DateTime, origin = "1970-01-01")
-s2$Start = as.POSIXct(s2$DateTime, origin = "1970-01-01")
-evdat = data.frame(s1$event, s2$Start, s1$End, s2$wmin, s1$wmax, s3$wmedian, s4$w08)
-colnames(evdat) = c("event", "Start", "End", "wmin", "wmax", "wmedian", "w08")
-evdat$Dur = as.numeric(difftime(evdat$End, evdat$Start))
-evdat$Day = format(evdat$Start, "%m-%d")
+evdat = aggregate(w ~ Ledge+e, data = dflong, FUN = function(x) quantile(x, probs = .8));  
+s2 = aggregate(DateTime ~ Ledge+e, data = dflong, FUN = min); 
+s3 = aggregate(DateTime ~ Ledge+e, data = dflong, FUN = max);
+evdat[,c("Start", "End")] = data.frame(s2$DateTime, s3$DateTime)
 
 
 # Weight as a function of time of the day etc. 
+evdat$Dur = difftime(evdat$End, evdat$Start, "secs")
+evdat$Day = format(evdat$Start, "%m-%d")
 evdat$H = as.numeric(format(evdat$Start, "%H"))
 evdat$daynum = as.numeric(format(evdat$Start, "%j"))
 evdat$day = as.Date(evdat$Start)
--- SummarizePlot.R
+++ SummarizePlot.R
@@ -12,24 +12,23 @@ library(ggplot2)
 start = evdat[1,"day"]
 end = evdat[nrow(evdat),"day"]
 titlestring = paste("Weights at Farallon 3", "between", start, "&", end, sep = " ")
-pd = subset(evdat, Dur > 6 & wmax < 1.2)
+pd = subset(evdat, Dur > 6 & w < 1.2 & Ledge == "Toms")
 n = nrow(pd)
-ggplot(data = pd, aes(x = wmax*1000)) + geom_histogram(fill = "red") + ggtitle(titlestring) + scale_x_continuous(name = "Max weight per event [g]") + theme_classic() + annotate("text", x = 800, y = 200, label = paste("n =", n)) 
+ggplot(data = pd, aes(x = w*1000)) + geom_histogram(fill = "red") + ggtitle(titlestring) + scale_x_continuous(name = "Max weight per event [g]") + theme_classic() + annotate("text", x = 800, y = 10, label = paste("n =", n)) 
 
 
 # NUMBER OF WEIGHT EVENTS OVER TIME                                                                                         
-nweight = as.data.frame(table(evdat$day))
-nweight$Var1 = as.Date(nweight$Var1)
-ggplot(nweight, aes(x = Var1, y = Freq)) + geom_bar(stat = "identity", fill = "lightblue", col = NA) + theme_classic() + scale_x_date(name = "Date") + scale_y_continuous(name = "Number of measurements")
+nweight = as.data.frame(table(evdat$Ledge, evdat$day))
+nweight$Var2 = as.Date(nweight$Var2)
+ggplot(nweight, aes(x = Var2, y = Freq)) + geom_bar(stat = "identity", fill = "lightblue", col = NA) + theme_classic() + scale_x_date(name = "Date") + scale_y_continuous(name = "Number of measurements") + facet_wrap(~Var1, scale = "free_y")
 
 # WEIGHT AS A FUNCTION OF SEASON
-pd = subset(evdat, w08 < 1.2)
-ggplot(pd, aes(x = daynum, group = daynum, y = w08)) + geom_boxplot(fill = "lightblue") + theme_classic()
+pd = subset(evdat, w < 1.2)
+ggplot(pd, aes(x = daynum, group = daynum, y = w)) + geom_boxplot(fill = "lightblue") + theme_classic() + facet_wrap(~Ledge)
 
 # WEIGHT AS A FUNCTION OF TIME OF THE DAY
-pd = subset(evdat, w08 < 1.2)
-ggplot(pd, aes(x = H, group = H, y = w08*1000)) + geom_boxplot(fill = "lightblue") + theme_classic() + scale_y_continuous(name = "Max weight per event [g]") + ggtitle("Weights per hour")
-pd = subset(evdat, w08 < 1.2)
+pd = subset(evdat, w < 1.2)
+ggplot(pd, aes(x = H, group = H, y = w*1000)) + geom_boxplot(fill = "lightblue") + theme_classic() + scale_y_continuous(name = "Max weight per event [g]") + ggtitle("Weights per hour") + facet_wrap(~Ledge)
 
 
 # Example weight event
